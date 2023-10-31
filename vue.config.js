07a9c49c8bdfcbdd3da70bead64e31c1670ec7bc
mkhong210 <ghdalsrud210@gmail.com>
2023-10-31 09:56:07 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // publicPath : '/레파지토리명'
+  // 이거 붙여서 build 한다음에 git에 배포
+  // publicPath:'/vue-test'
+  // netlify에서는 필요없움
 })
